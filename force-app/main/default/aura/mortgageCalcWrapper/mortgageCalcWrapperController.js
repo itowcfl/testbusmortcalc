4fb89a2eb4ac5750ffc1aa663015099b7698e4be
itowcfl <itowery@communityHDC.org>
2024-08-06 14:07:02 -0700

    Update mortgageCalcWrapperController.js

--- force-app/main/default/aura/mortgageCalcWrapper/mortgageCalcWrapperController.js
+++ force-app/main/default/aura/mortgageCalcWrapper/mortgageCalcWrapperController.js
@@ -1,9 +1,9 @@
 ({
-    handleMortgageChange : function(component, event) {
-        /* when a mortgage field changes, update the aura component attributes so that when passed back to the flow */
+    handleBusinessLoanChange : function(component, event) {
+        /* when a business loan field changes, update the aura component attributes so that when passed back to the flow */
         let eventParams = event.getParams('detail');
-        component.set("v.mortgageRate", eventParams.rate);
-        component.set("v.mortgageYears", eventParams.years);
-        component.set("v.mortgagePrincipal", eventParams.principal);
+        component.set("v.businessLoanRate", eventParams.rate);
+        component.set("v.businessLoanYears", eventParams.years);
+        component.set("v.businessLoanPrincipal", eventParams.principal);
     }
 })
